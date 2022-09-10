2f84ec41110c43b4b2679009e9fd1def537ef15c
Dániel Horváth <horvath.daniel@lexunit.hu>
2022-09-10 20:56:13 +0200

    demo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,13 @@
 import { Component } from '@angular/core';
+import { AppService } from './app.service';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {}
+export class AppComponent {
+  constructor(private appService: AppService) {
+    console.log(appService.getHello());
+  }
+}
--- vite.config.ts
+++ vite.config.ts
@@ -1,30 +1,6 @@
 import { defineConfig } from 'vite';
-import { ViteAngularPlugin } from '@nxext/angular-vite';
+import { vpa } from '@nitedani/vite-plugin-angular';
 
 export default defineConfig({
-  esbuild: false,
-  plugins: [ViteAngularPlugin()],
-  resolve: {
-    // preserveSymlinks: true,
-  },
-  // esbuild: {
-  //   sourcemap: false,
-  //   legalComments: 'none',
-  // },
-  build: {
-    minify: true,
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          vendor: [
-            '@angular/core',
-            '@angular/common',
-            '@angular/platform-browser',
-            '@angular/platform-browser-dynamic',
-            'zone.js/dist/zone',
-          ],
-        },
-      },
-    },
-  },
+  plugins: [vpa()],
 });
